2cb4afe80193e602c6ebc4fadc49b2517c612f89
eXmendiC <exmendic@gmail.com>
2020-05-21 13:48:28 +0200

    Add files via upload

---
+++ Shift_SubTVtoBD.bat
@@ -0,0 +1,43 @@
+@echo off
+:: This script is for putting TV subs from a TV show onto a Blu-ray source with correct timing
+REM ######################
+setlocal ENABLEDELAYEDEXPANSION
+echo.
+echo Keep all files and this batch file in the same folder. 
+echo Only set the names of them, no directory structure (paths)!
+echo Just press enter for detault values.
+echo Always use lowercase.
+echo.
+REM ######################
+set template=template_clean.ass
+REM ######################
+:anew
+set /p srcname=TV-Source (e.g. TestTV.mkv): 
+set /p scriptname=TV-Subtitle (e.g. TestTV.ass): 
+set /p dstname=BD-Source (e.g. TestBD.mkv): 
+echo.
+:: That python script is and replacing the font
+py -3 audio\prass\prass.py copy-styles --resample --from audio\template_clean.ass --to "%scriptname%" -o "%scriptname%_sfx.ass"
+
+:: Shifting the subtitles
+py -2 audio\sushi\sushi.py --src "%srcname%" --src-keyframes auto --dst "%dstname%" --dst-keyframes auto --kf-mode all --max-ts-duration 0.5 --max-ts-distance 1 --script "%scriptname%_sfx.ass" --max-kf-distance 2.5 -o "%scriptname%-sushi.ass"
+
+:: Some additional timing fixing if sushi isn't accurate enough
+REM "%scriptname%-sushi.ass" "%scriptname%-sushi2.ass"
+REM py -3 audio\prass\prass.py tpp "%scriptname%-sushi2.ass" --lead-in 43 --lead-out 43 --gap 210 --overlap 126 --bias 60 --keyframes "%dstname%.sushi.keyframes.txt" --fps 23.976 --kf-before-start 210 --kf-before-end 294 --kf-after-start 294 --kf-after-end 294 -o "%scriptname%-sushi.ass"
+
+:: Muxing the subtitles with the Blu-ray video
+:: You might want to change the "--language" here
+mkvmerge -o "%dstname%_final.mkv" "%dstname%" "--language" "0:ger" "--track-name" "0:Subs" "--default-track" "0:yes" "%scriptname%-sushi.ass" "--attachment-mime-type" "application/vnd.ms-opentype" "--attachment-name" "font.ttf" "--attach-file" "audio\font.ttf" "--attachment-mime-type" "application/vnd.ms-opentype" "--attachment-name" "font2.ttf" "--attach-file" "audio\font2.ttf"
+
+:: Deleting everything that isn't needed anymore
+del "%srcname%_fixed.mkv.sushi.keyframes.txt"
+del "%srcname%.sushi.keyframes.txt"
+del "%dstname%.sushi.keyframes.txt"
+del "%scriptname%-sushi2.ass"
+del "%srcname%_fixed.mkv"
+del "%scriptname%_sfx.ass"
+
+echo Done.
+goto anew
+PAUSE
