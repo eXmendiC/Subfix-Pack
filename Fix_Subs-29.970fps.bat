8d0c75e35e0c98bdf58cd7e53ae70a95497f4544
eXmendiC <exmendic@gmail.com>
2020-05-23 16:25:04 +0000

    Update Fix_Subs-29.970fps.bat

--- Fix_Subs-29.970fps.bat
+++ Fix_Subs-29.970fps.bat
@@ -1,10 +1,8 @@
 @echo off
 :: This script is for subtitles from different sources (timing & font).
-:: For "extract=y" you need the following base as a MKV: "Video" + "Audio: Japdub" + "Sub: Full"
-:: For other languages than German the script needs a bit tweaking (see the comments below)
+:: For "extract=y" you need the following base as a MKV: "ID0:Video" + "ID1:Audio" + "ID2:Sub"
 REM ######################
 setlocal ENABLEDELAYEDEXPANSION
-echo This script is for fixing subs.
 echo.
 echo Keep all files and this batch file in the same folder. 
 echo Only set the names of them, no directory structure (paths)!
@@ -12,31 +10,14 @@ echo Just press enter for detault values.
 echo Always use lowercase.
 echo.
 REM ######################
-:: Explanation:
-:: fast = skip everything except video/subtitle input: "y" or "n"
-:: extract = drag and drop mode, recommended if the subtitles are inside the .mkv file: "y" or "n"
-:: source = subtitle format: "ass" and "srt" supported
-:: timing = shift the subtitles by frames: "-2","-1","0","1","2" supported
-:: sush = use sushi for fixing the timing of the subtitle: "y" or "n"
-:: sushmode = decide the modus of sushi: 
-:: Mode 1 - Fixing one frame gaps
-:: Mode 2 - Fixing bad timing (CR)
-:: Mode 3 - Fixing bad timing (AMZ)
-:: Mode 4 - Fixing bad timing (NF)
-:: Mode 5 - Fixing bad timing (FUNi)
-:: Mode 6 - Fixing bad timing (HiDV)
-:: secpass = Forces audio sync and fps for better results and removes muxed subtitles before fixing subtitles: "y" or "n"
-:: mux = mux the final result into a mkv file: "y" or "n"
-:: template = template.ass file that stores all styles, inside the "audio" folder
-:: font = the font for your default styling, inside the "audio" folder
-:: font2 = the font for your italic styling, inside the "audio" folder
+:: Explanation: https://iamscum.wordpress.com/guides/prass/
 REM ######################
 set fast=n
 set extract=n
 set source=ass
-set timing=0
-set sush=y
-set sushmode=3
+set timefix=y
+set timefixmode=3
+set shifting=0
 set secpass=y
 set mux=y
 set template=template_advanced.ass
@@ -45,10 +26,18 @@ set font2=font1i.ttf
 REM ######################
 
 if "%extract%" EQU "y" (
-mkvextract --ui-language en tracks "%~1" 2:"%~n1.ass"
-set videoname=%~n1.mkv
-set scriptname=%~n1.ass
-goto TTT
+ if "%source%" EQU "ass" (
+ mkvextract --ui-language en tracks "%~1" 2:"%~n1.ass"
+ set videoname=%~n1.mkv
+ set scriptname=%~n1.ass
+ goto TTT
+ )
+ if "%source%" EQU "srt" (
+ mkvextract --ui-language en tracks "%~1" 2:"%~n1.srt"
+ set videoname=%~n1.mkv
+ set scriptname=%~n1.srt
+ goto TTT
+ )
 )
 
 set /p videoname=Video (e.g. Test.mkv): 
@@ -70,12 +59,14 @@ echo Default is ass
 set /p source=srt or ass input (e.g. ass): 
 echo Default is n
 set /p secpass=Use a second mux pass (y/n): 
-echo Only works with ASS input. Default is 0
-set /p timing=Time difference for subtitles (1,2,24 for frame/s forward, 0 for nothing or -1,-2,-24 for frame/s backward): 
+echo Default is 0
+set /p shifting=Time difference for subtitles (1,2,24 for frame/s forward, 0 for nothing or -1,-2,-24 for frame/s backward): 
 echo Default is y
-set /p sush=Run sushi (y/n): 
-echo Only works if sushi is y. Higher value means more strength. Default is 3 for srt and 1 for ass
-set /p sushmode=Sushi Mode (0-6): 
+set /p timefix=Run timefix (y/n): 
+ if "%timefix%" EQU "y" (
+ echo Default is 3
+ set /p timefixmode=Strenght of the timefix (0-6): 
+)
 echo Default is y
 set /p mux=Mux everything together at the end (y/n): 
 
@@ -101,91 +92,100 @@ if "%source%" EQU "srt" (
  py -3 audio\amazon-netflix_typeset_split.py "%scriptname%_srt.ass" "%scriptname%_tmp.ass"
  del "%scriptname%_srt.ass"
  echo Converting completed.
- goto AMZ
 )
 
-if "%timing%" EQU "0" (
- py -3 audio\prass\prass.py copy-styles --resample --from audio\%template% --to "%scriptname%" -o "%scriptname%_tmp.ass"
- goto AMZ
+if "%source%" EQU "ass" (
+py -3 audio\prass\prass.py copy-styles --resample --from audio\%template% --to "%scriptname%" -o "%scriptname%_tmp.ass"
 )
 
-if "%timing%" EQU "1" (
- py -3 audio\prass\prass.py copy-styles --resample --from audio\%template% --to "%scriptname%" | py -3 audio\prass\prass.py shift --by 33ms -o "%scriptname%_tmp.ass"
- goto AMZ
+if "%shifting%" EQU "1" (
+ ren "%scriptname%_tmp.ass" "%scriptname%_tmp2.ass"
+ py -3 audio\prass\prass.py shift --by 33ms "%scriptname%_tmp2.ass" -o "%scriptname%_tmp.ass"
+ del "%scriptname%_tmp2.ass"
 )
 
-if "%timing%" EQU "-1" (
- py -3 audio\prass\prass.py copy-styles --resample --from audio\%template% --to "%scriptname%" | py -3 audio\prass\prass.py shift --by -33ms -o "%scriptname%_tmp.ass"
- goto AMZ
+if "%shifting%" EQU "-1" (
+ ren "%scriptname%_tmp.ass" "%scriptname%_tmp2.ass"
+ py -3 audio\prass\prass.py shift --by -33ms "%scriptname%_tmp2.ass" -o "%scriptname%_tmp.ass"
+ del "%scriptname%_tmp2.ass"
 )
 
-if "%timing%" EQU "2" (
- py -3 audio\prass\prass.py copy-styles --resample --from audio\%template% --to "%scriptname%" | py -3 audio\prass\prass.py shift --by 66ms -o "%scriptname%_tmp.ass"
- goto AMZ
+if "%shifting%" EQU "2" (
+ ren "%scriptname%_tmp.ass" "%scriptname%_tmp2.ass"
+ py -3 audio\prass\prass.py shift --by 66ms "%scriptname%_tmp2.ass" -o "%scriptname%_tmp.ass"
+ del "%scriptname%_tmp2.ass"
 )
 
-if "%timing%" EQU "-2" (
- py -3 audio\prass\prass.py copy-styles --resample --from audio\%template% --to "%scriptname%" | py -3 audio\prass\prass.py shift --by -66ms -o "%scriptname%_tmp.ass"
- goto AMZ
+if "%shifting%" EQU "-2" (
+ ren "%scriptname%_tmp.ass" "%scriptname%_tmp2.ass"
+ py -3 audio\prass\prass.py shift --by -66ms "%scriptname%_tmp2.ass" -o "%scriptname%_tmp.ass"
+ del "%scriptname%_tmp2.ass"
 )
 
-if "%timing%" EQU "24" (
- py -3 audio\prass\prass.py copy-styles --resample --from audio\%template% --to "%scriptname%" | py -3 audio\prass\prass.py shift --by 792ms -o "%scriptname%_tmp.ass"
- goto AMZ
+if "%shifting%" EQU "24" (
+ ren "%scriptname%_tmp.ass" "%scriptname%_tmp2.ass"
+ py -3 audio\prass\prass.py shift --by 792ms "%scriptname%_tmp2.ass" -o "%scriptname%_tmp.ass"
+ del "%scriptname%_tmp2.ass"
 )
 
-if "%timing%" EQU "-24" (
- py -3 audio\prass\prass.py copy-styles --resample --from audio\%template% --to "%scriptname%" | py -3 audio\prass\prass.py shift --by -792ms -o "%scriptname%_tmp.ass"
- goto AMZ
+if "%shifting%" EQU "-24" (
+ ren "%scriptname%_tmp.ass" "%scriptname%_tmp2.ass"
+ py -3 audio\prass\prass.py shift --by -792ms "%scriptname%_tmp2.ass" -o "%scriptname%_tmp.ass"
+ del "%scriptname%_tmp2.ass"
 )
 
-:AMZ
 awk "/\[Script Info\]/ { print; print \"ScaledBorderAndShadow: yes\"; next }1" "%scriptname%_tmp.ass" >"%scriptname%_tmp2.ass"
 del "%scriptname%_tmp.ass"
 ren "%scriptname%_tmp2.ass" "%scriptname%_tmp.ass"
 
-if "%sush%" EQU "n" (
-set set sushmode=0
+if "%timefix%" EQU "n" (
+ ren "%scriptname%_tmp.ass" "%scriptname%_fixed.ass"
+ goto NSU
 )
-
-if "%sushmode%" EQU "0" (
+if "%timefixmode%" EQU "0" (
  ren "%scriptname%_tmp.ass" "%scriptname%_fixed.ass"
  goto NSU
 )
 
-if "%sush%" EQU "y" (
+if "%timefix%" EQU "y" (
  if NOT exist "%videoname%_fixed.mkv_keyframes.txt" (
   echo Generate keyframes...
   ffmpeg -i "%videoname%_fixed.mkv" -f yuv4mpegpipe -vf scale=640:360 -pix_fmt yuv420p -vsync drop - | SCXvid "%videoname%_fixed.mkv_keyframes.txt"
   echo Keyframes completed.
   )
- if "%sushmode%" EQU "1" (
+ if "%timefixmode%" EQU "1" (
   py -3 audio\prass\prass.py tpp "%scriptname%_tmp.ass" --keyframes "%videoname%_fixed.mkv_keyframes.txt" --fps 29.970 --kf-before-start 132 --kf-before-end 132 --kf-after-start 132 --kf-after-end 132    -o "%scriptname%_fixed.ass"
   del "%scriptname%_tmp.ass"
  )
- if "%sushmode%" EQU "2" (
+ if "%timefixmode%" EQU "2" (
   py -3 audio\prass\prass.py tpp "%scriptname%_tmp.ass" --lead-in 66 --lead-out 66 --gap 320 --overlap 198 --bias 50 --keyframes "%videoname%_fixed.mkv_keyframes.txt" --fps 29.970 --kf-before-start 320 --kf-before-end 320 --kf-after-start 386 --kf-after-end 386    -o "%scriptname%_fixed.ass"
   del "%scriptname%_tmp.ass"
  )
- if "%sushmode%" EQU "3" (
+ if "%timefixmode%" EQU "3" (
   py -3 audio\prass\prass.py tpp "%scriptname%_tmp.ass" --lead-in 132 --lead-out 132 --gap 716 --overlap 396 --bias 80 --keyframes "%videoname%_fixed.mkv_keyframes.txt" --fps 29.970 --kf-before-start 320 --kf-before-end 452 --kf-after-start 452 --kf-after-end 452    -o "%scriptname%_fixed.ass"
   del "%scriptname%_tmp.ass"
  )
- if "%sushmode%" EQU "4" (
+ if "%timefixmode%" EQU "4" (
   py -3 audio\prass\prass.py tpp "%scriptname%_tmp.ass" --lead-in 132 --lead-out 132 --gap 320 --overlap 198 --bias 100 --keyframes "%videoname%_fixed.mkv_keyframes.txt" --fps 29.970 --kf-before-start 320 --kf-before-end 452 --kf-after-start 452 --kf-after-end 452    -o "%scriptname%_fixed.ass"
   del "%scriptname%_tmp.ass"
  )
- if "%sushmode%" EQU "5" (
+ if "%timefixmode%" EQU "5" (
   py -3 audio\prass\prass.py tpp "%scriptname%_tmp.ass" --lead-in 66 --lead-out 66 --gap 518 --overlap 396 --bias 60 --keyframes "%videoname%_fixed.mkv_keyframes.txt" --fps 29.970 --kf-before-start 320 --kf-before-end 452 --kf-after-start 386 --kf-after-end 386    -o "%scriptname%_fixed.ass"
   del "%scriptname%_tmp.ass"
  )
- if "%sushmode%" EQU "6" (
+ if "%timefixmode%" EQU "6" (
   py -3 audio\prass\prass.py tpp "%scriptname%_tmp.ass" --lead-in 66 --lead-out 99 --gap 396 --overlap 262 --bias 70 --keyframes "%videoname%_fixed.mkv_keyframes.txt" --fps 29.970 --kf-before-start 320 --kf-before-end 320 --kf-after-start 386 --kf-after-end 386    -o "%scriptname%_fixed.ass"
   del "%scriptname%_tmp.ass"
  )
 )
 
 :NSU
+
+if "%typo%" EQU "y" (
+ ren "%scriptname%_fixed.ass" "%scriptname%-needfix.ass"
+ py -3 audio\fuehre_mich.py "%scriptname%-needfix.ass" "%scriptname%_fixed.ass"
+ del "%scriptname%-needfix.ass"
+)
 
 if "%mux%" EQU "y" (
  mkvmerge -o "%videoname%_final.mkv" "%videoname%_fixed.mkv" "--sub-charset" "0:UTF-8" "--language" "0:zxx" "--track-name" "0:" "--default-track" "0:yes" "%scriptname%_fixed.ass" "--attachment-mime-type" "application/vnd.ms-opentype" "--attachment-name" "%font%" "--attach-file" "audio\%font%" "--attachment-mime-type" "application/vnd.ms-opentype" "--attachment-name" "%font2%" "--attach-file" "audio\%font2%"
