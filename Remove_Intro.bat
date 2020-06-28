dc529014707886cd6cd229187d3656dc358d7275
eXmendiC <exmendic@gmail.com>
2020-06-28 14:15:21 +0000

    Update Remove_Intro.bat

--- Remove_Intro.bat
+++ Remove_Intro.bat
@@ -2,6 +2,7 @@
 :: This script is for removing intros of web sources
 :: Drag & Drop the MKV above it
 :: Change "time=00:00:05.005" (HH:MM:SS.ms) when the intro is over
+:: Set "encode=y" if the results are too inacurate - this takes longer
 REM ######################
 setlocal ENABLEDELAYEDEXPANSION
 echo.
@@ -10,6 +11,12 @@ echo.
 REM ######################
 :: Change this values to your liking
 set time=00:00:05.005
+set encode=n
 REM ######################
+if "%encode%" EQU "y" (
+:: Add "-vf scale=1280x720" before "-c:v libx264", change the preset or raise the crf value if you want to trade accuracy for speed
+ffmpeg -i "%~1" -ss %time% -c:v libx264 -preset veryfast -crf 20 -c:a copy -c:s copy "[NoIntroEnc] %~n1.mkv"
+) else (
 mkvmerge.exe --ui-language en --output "[NoIntro] %~n1.mkv" "(" "%~1" ")" --split parts:%time%-59:59:59.999
+)
 Pause
