9e12731f94f423e2f925905e689d7f25c17427e6
eXmendiC <exmendic@gmail.com>
2020-07-07 20:43:44 +0000

    Update Remove_Intro.bat

--- Remove_Intro.bat
+++ Remove_Intro.bat
@@ -9,6 +9,8 @@ echo.
 echo Keep all files and this batch file in the same folder. 
 echo.
 REM ######################
+IF [%1]==[] echo "Drag & Drop the file above it!" && goto end
+REM ######################
 :: Change this values to your liking
 set time=00:00:05.005
 set encode=n
@@ -19,4 +21,5 @@ ffmpeg -i "%~1" -ss %time% -c:v libx264 -preset veryfast -crf 20 -c:a copy -c:s 
 ) else (
 mkvmerge.exe --ui-language en --output "[NoIntro] %~n1.mkv" "(" "%~1" ")" --split parts:%time%-59:59:59.999
 )
+:end
 Pause
