d5ac78df6b5b583fe2724c541b0a211371394894
eXmendiC <exmendic@gmail.com>
2020-07-30 10:37:42 +0000

    Update Shift_AudioToBD.bat

--- Shift_AudioToBD.bat
+++ Shift_AudioToBD.bat
@@ -25,30 +25,20 @@ move /Y "%srcname%_temp.mka" audio\sync-audio-tracks
 move /Y "want_sync.wav" audio\sync-audio-tracks
 move /Y "have_sync.wav" audio\sync-audio-tracks
 
-:: Finding out the offset/delay
+:: Generate offset, convert seconds to milliseconds and store them in a text file
 cd audio\sync-audio-tracks
-wsl bash -i compute-sound-offset.sh want_sync.wav have_sync.wav 0 >> offset.txt
+wsl offset=$(bash -i compute-sound-offset.sh want_sync.wav have_sync.wav 0) ; offset=$(awk "BEGIN {print ($offset*1000)}") ; offset=$(sed "s/\.[^.]*$//" ^<^<^< "$offset") ; echo "$offset" >> offset.txt
 del want_sync.wav
 del have_sync.wav
 
-:: Storing the offset value inside a variable
-wsl sed 's/[^a-zA-Z^ \t^.]*//g' offset.txt >> offset_fixed.txt
+:: Store delay inside variable
+set /p delay=< offset.txt
+echo %delay%
 del offset.txt
-wsl sed 's/^000.*//' offset_fixed.txt >> offset_fixed2.txt
-del offset_fixed.txt
-set /p delay=< offset_fixed2.txt
-del offset_fixed2.txt
-
-::Reducing the amount of numbers - else it won't work
-if "%delay%"=="%delay:/-=%" ( 
-set delay=%delay:~0,6%
-) else ( 
-set delay=%delay:~0,5%
-)
 
 :: Muxing it with a delay (no quality loss since no converting)
-mkvmerge --ui-language en --output "[SyncedTo] %dstname%.mka" --default-track "0:yes" --track-name "0:%delay%ms" --sync "0:%delay%" "(" "%srcname%_temp.mka" ")"
+mkvmerge --ui-language en --output "%dstname% [%delay%ms].mka" --default-track "0:yes" --track-name "0:%delay%ms" --sync "0:%delay%" "(" "%srcname%_temp.mka" ")"
 del "%srcname%_temp.mka"
-move /Y "[SyncedTo] %dstname%.mka" ..\..
+move /Y "%dstname% [%delay%ms].mka" ..\..
 echo The delay is %delay%ms
 PAUSE
