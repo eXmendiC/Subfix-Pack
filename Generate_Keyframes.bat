d734f961c7d4044c54b9307e7f10901c344d07e3
eXmendiC <exmendic@gmail.com>
2020-08-04 18:51:50 +0200

    Add files via upload

---
+++ Generate_Keyframes.bat
@@ -0,0 +1,14 @@
+@echo off
+:: Drag & Drop the MKV above it
+:: This script generates keyframes for you.
+REM ######################
+setlocal ENABLEDELAYEDEXPANSION
+echo.
+echo Keep all files and this batch file in the same folder. 
+echo.
+REM ######################
+IF [%1]==[] echo "Drag & Drop the file above it!" && goto end
+REM ######################
+ffmpeg -i "%~1" -f yuv4mpegpipe -vf scale=640:360 -pix_fmt yuv420p -vsync drop - | SCXvid.exe "%~n1_keyframes.txt"
+echo Done.
+PAUSE
