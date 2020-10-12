d7551f2bb128f929ce9d0c2f06ede5cbc1285694
eXmendiC <exmendic@gmail.com>
2020-10-12 10:57:14 +0000

    Update Fix_Subs.bat

--- Fix_Subs.bat
+++ Fix_Subs.bat
@@ -154,15 +154,15 @@ if "%timefixing%" EQU "y" (
   echo Keyframes completed.
   )
  if "%timefixingmode%" EQU "1" (
-  py -3 audio\prass\prass.py tpp "%scriptname%_tmp.ass" --keyframes "%videoname%_fixed.mkv_keyframes.txt" --fps 23.976 --kf-before-start 84 --kf-before-end 84 --kf-after-start 84 --kf-after-end 84 -o "%scriptname%_fixed.ass"
-  del "%scriptname%_tmp.ass"
- )
- if "%timefixingmode%" EQU "2" (
   py -3 audio\prass\prass.py tpp "%scriptname%_tmp.ass" --lead-in 42 --lead-out 42 --gap 210 --overlap 126 --bias 50 --keyframes "%videoname%_fixed.mkv_keyframes.txt" --fps 23.976 --kf-before-start 210 --kf-before-end 252 --kf-after-start 210 --kf-after-end 252 -o "%scriptname%_fixed.ass"
   del "%scriptname%_tmp.ass"
  )
+ if "%timefixingmode%" EQU "2" (
+  py -3 audio\prass\prass.py tpp "%scriptname%_tmp.ass" --lead-in 42 --lead-out 42 --gap 336 --overlap 210 --bias 60 --keyframes "%videoname%_fixed.mkv_keyframes.txt" --fps 23.976 --kf-before-start 252 --kf-before-end 294 --kf-after-start 252 --kf-after-end 294 -o "%scriptname%_fixed.ass"
+  del "%scriptname%_tmp.ass"
+ )
  if "%timefixingmode%" EQU "3" (
-  py -3 audio\prass\prass.py tpp "%scriptname%_tmp.ass" --lead-in 84 --lead-out 84 --gap 462 --overlap 252 --bias 80 --keyframes "%videoname%_fixed.mkv_keyframes.txt" --fps 23.976 --kf-before-start 252 --kf-before-end 294 --kf-after-start 252 --kf-after-end 294 -o "%scriptname%_fixed.ass"
+  py -3 audio\prass\prass.py tpp "%scriptname%_tmp.ass" --lead-in 84 --lead-out 84 --gap 462 --overlap 252 --bias 80 --keyframes "%videoname%_fixed.mkv_keyframes.txt" --fps 23.976 --kf-before-start 294 --kf-before-end 294 --kf-after-start 294 --kf-after-end 294 -o "%scriptname%_fixed.ass"
   del "%scriptname%_tmp.ass"
  )
  if "%timefixingmode%" EQU "4" (
