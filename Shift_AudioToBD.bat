305ffe38096d48174c89133f89a6ba60898a78f6
eXmendiC <exmendic@gmail.com>
2022-04-07 16:53:35 +0000

    Update Shift_AudioToBD.bat
    Changing the sorting (V=Video, A=Audio, S=Subtitle) => From "V-S-A" to "V-A-S".

--- Shift_AudioToBD.bat
+++ Shift_AudioToBD.bat
@@ -9,7 +9,7 @@ echo Just press enter for detault values.
 echo Always use lowercase.
 echo.
 REM ######################
-:: Explanation & Requirements: https://iamscum.wordpress.com/guides/audio/
+:: Explanation & Requirements: https://iamscum.wordpress.com/guides/vfr/
 REM ######################
 :: Change this values to your liking
 set replace=n
@@ -42,7 +42,7 @@ set /p delay=< "%is_sync%".txt
 del "%is_sync%".txt
 
 :: Muxing it with a delay (no quality loss since no converting)
-mkvmerge --ui-language en --output "%is_sync% [%delay%ms].mka" --default-track "0:yes" --track-name "0:%delay%ms" --sync "0:%delay%" "(" "%want_sync%_temp.mka" ")"
+mkvmerge --ui-language en --output "%is_sync% [%delay%ms].mka" --default-track "0:no" --track-name "0:Shifted by %delay%ms" --sync "0:%delay%" "(" "%want_sync%_temp.mka" ")"
 del "%want_sync%_temp.mka"
 move /Y "%is_sync% [%delay%ms].mka" ..\..
 echo The delay is %delay%ms
@@ -50,13 +50,13 @@ cd ..\..
 
 :: Audio replacing
 if "%replace%" EQU "y" (
-mkvmerge.exe -o "[NEW] %is_sync%" "--no-audio"  "(" "%is_sync%" ")"  "--language" "1:jpn" "(" "%is_sync% [%delay%ms].mka" ")"
+mkvmerge.exe -o "[NEW] %is_sync%" --no-audio --no-subtitles --no-buttons --no-track-tags --no-chapters --no-attachments --no-global-tags "(" "%is_sync%" ")"  --default-track "0:yes" "--language" "1:jpn" "(" "%is_sync% [%delay%ms].mka" ")" --no-video --no-audio  "(" "%is_sync%" ")"
 del "%is_sync% [%delay%ms].mka"
 )
 
 :: Audio adding
 if "%add%" EQU "y" (
-mkvmerge.exe -o "[NEW] %is_sync%" "(" "%is_sync%" ")"  "--default-track" "0:yes" "--language" "0:ger" "(" "%is_sync% [%delay%ms].mka" ")"
+mkvmerge.exe -o "[NEW] %is_sync%" --no-audio --no-subtitles --no-buttons --no-track-tags --no-chapters --no-attachments --no-global-tags "(" "%is_sync%" ")"  --default-track "0:yes" "--language" "1:ger" "(" "%is_sync% [%delay%ms].mka" ")" --no-video "(" "%is_sync%" ")"
 del "%is_sync% [%delay%ms].mka"
 )
 
